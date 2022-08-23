cdd9503813f29c0a1aa71b14fd51097c4e7564a0
Anthony Barragan <anthony.bar.89@gmail.com>
2022-08-22 20:08:48 -0700

    Feature: added snake page and link. About to start setting up TypeScript and Socket.io

---
+++ pages/snakeLobby.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+function SnakeLobby() {
+    return (
+        <>
+            <Head>
+                <title>Snake Game</title>
+                <meta name="keywords" content="math games, classroom, educational, teacher, student" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <p>Youre in the snake lobby</p>
+        </>
+    )
+}
+
+export default SnakeLobby;
